ad9e28fcb5c21e9f9b9c7f6efc15bef47cbc3c89
Sabi <sabinoquiros22@gmail.com>
2023-08-21 11:20:18 +0200

    connection refactor and handling of 1001 event

--- src/modules/connect.js
+++ src/modules/connect.js
@@ -5,53 +5,68 @@ import send_message from '../workers/send_message.js';
 
 import logger from '../helpers/logger.js';
 
-const gateway_url = 'wss://gateway.discord.gg/?encoding=json&v=9';
-var ws;
+const GATEWAY_URL = 'wss://gateway.discord.gg/?encoding=json&v=9';
+const RECONNECT_DELAY = 2000; 
 
-export default () => {
-    ws = new web_socket(gateway_url);
+export default function init_webSocket() {
+    const ws = new web_socket(GATEWAY_URL);
 
-    ws.on('open', () => {
+    ws.on('open', handle_open);
+    ws.on('message', handle_message);
+    ws.on('error', handle_error);
+    ws.on('close', handle_close);
+
+    function handle_open() {
         logger('Connected to Discord gateway.');
-    });
-
-    ws.on('message', (data) => {
-        if (Array.isArray(data)) {
-            for (let message of data) {
-                handle_message(message);
-            }
-        } else {
-            handle_message(data);
-        }
-    });
+    }
 
     function handle_message(data) {
-        const message = JSON.parse(data);
-        process_message(message);
+        if (Array.isArray(data)) {
+            data.forEach(process_data);
+        } else {
+            process_data(data);
+        }
     }
 
-    function process_message(message) {
-        if (message.op === 10) { 
-            setInterval(() => {
-                ws.send(JSON.stringify({ op: 1, d: null })); 
-            }, message.d.heartbeat_interval);
-    
-            ws.send(JSON.stringify(ws_auth_payload()));
+    function handle_error(error) {
+        logger('WebSocket Error:', error);
+    }
+
+    function handle_close(code, reason) {
+        logger(`WebSocket Closed. Code: ${code}, Reason: ${reason}`);
+        if (code === 1001) {
+            setTimeout(init_webSocket, RECONNECT_DELAY);
+        }
+    }
+
+    function process_data(data) {
+        let message;
+        try {
+            message = JSON.parse(data);
+        } catch (error) {
+            logger('Failed to parse message:', error);
+            return;
+        }
+        
+        const { op, t, d } = message;
+
+        if (op === 10) {
+            maintain_heartbeat(d.heartbeat_interval);
+            authenticate();
         }
 
-        if (message.t === 'MESSAGE_CREATE' && message.d.guild_id === process.env.discord_guild) {
-            send_message(message.d);
-        }    
+        if (t === 'MESSAGE_CREATE' && d.guild_id === process.env.discord_guild) {
+            send_message(d);
+        }
     }
 
-    ws.on('error', (error) => {
-        logger('WebSocket Error:', error);
-    });
-    
-    ws.on('close', (code, reason) => {
-        logger(`WebSocket Closed. Code: ${code}, Reason: ${reason}`);
-    });
-    
+    function maintain_heartbeat(interval) {
+        setInterval(() => {
+            ws.send(JSON.stringify({ op: 1, d: null }));
+        }, interval);
+    }
+
+    function authenticate() {
+        ws.send(JSON.stringify(ws_auth_payload()));
+    }
 }
-
-
